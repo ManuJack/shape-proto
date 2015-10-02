ba870f644509530edfca09579018c6e435aa2de5
Emmanuel/ <ejacquier@tribalnova.com>
2015-10-02 13:35:20 -0400

    added utility scripts

---
+++ Assets/Scripts/Managers/ShapeSpawnerManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeSpawnerManager : MonoBehaviour 
+{
+    public GameObject[] prefabs;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    public void SpawnShape()
+    {
+        GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];
+        GameObject.Instantiate(prefab);
+    }
+    
+}
---
+++ Assets/Scripts/SpawnOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnClick : MonoBehaviour 
+{
+    public ShapeSpawnerManager Spawner;
+
+    void OnMouseDown()
+    {
+        Spawner.SpawnShape();
+    }
+}
